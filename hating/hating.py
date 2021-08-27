e9b3fe1765b136e16da62225561de2e2d957466d
Kebelei Csaba <71121076+kebeleicsaba@users.noreply.github.com>
2021-08-27 11:38:54 +0200

    Update hating.py

--- hating/hating.py
+++ hating/hating.py
@@ -1,6 +1,18 @@
 import json
 
-def init():
-    return json.load(open('hating/data.json'))
+class Genom:
+    def __init__(self) -> None:
+        d = json.load(open('data.json'))
+        self.connections = d["connections"]
+        self.letters = d["basic letters"]
 
+    def getHate(self, a: str, b: str):
+        for con in self.connections:
+            if {a, b} == set(con["letters"]) and len(set(con["letters"])) == 2:
+                return con["value"]
 
+    def fitness(self): 
+        pass
+
+g = Genom()
+print(g.getHate("A", "B"))
