2c7c4129960b095a9e235a13eee7a440e5fec051
hbela <hbela2000@gmail.com>
2021-08-27 12:38:01 +0200

    4

--- hating/hating2.py
+++ hating/hating2.py
@@ -1,18 +1,32 @@
 import random
 import json
 
-def init():
-    return json.load(open('hating/data.json'))
+f = json.load(open('hating/data.json'))
 
-data = init()["basic letters"]
-data2 = ["A","D","C","B","E"]
+data = f["basic letters"]
+data2 = ["A", "D", "C", "B", "E"]
+
 
 def mutate(genom):
-   r,r2 = (random.randint(0,len(genom)-1),random.randint(0,len(genom)-1))
-   while r == r2:
-       rand2 = random.randint(0,len(genom)-1)
-   swap = genom[r]
-   genom[r] = genom[r2]
-   genom[r2] = swap
+    r, r2 = (random.randint(0, len(genom)-1), random.randint(0, len(genom)-1))
+    while r == r2:
+        rand2 = random.randint(0, len(genom)-1)
+    swap = genom[r]
+    genom[r] = genom[r2]
+    genom[r2] = swap
 
-   return genom
+    return genom
+
+
+def crossover(genom, genom2):
+    new_genom = genom[:2]
+
+    while len(new_genom) != 5:
+        r = random.randint(0, len(genom)-1)
+        if genom2[r] not in new_genom:
+            new_genom.append(genom2[r])
+
+    return new_genom
+
+
+    
