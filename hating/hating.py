792e40e136d0f758b7887fdaeed5dbf3bf026d73
kebeleicsaba <kebeleicsaba@gmail.com>
2021-08-27 12:40:23 +0200

    Add crossover

--- hating/hating.py
+++ hating/hating.py
@@ -41,6 +41,16 @@ def mutate(genom) -> list:
  
     return genom
 
+def crossover(genom, genom2):
+    new_genom = genom[:2]
+
+    while len(new_genom) != 5:
+        r = random.randint(0, len(genom)-1)
+        if genom2[r] not in new_genom:
+            new_genom.append(genom2[r])
+
+    return new_genom
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
